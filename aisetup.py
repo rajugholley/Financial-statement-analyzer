3fd226bd765a464e593522424c5ba0a462caa204
rajugholley <raju.messi@gmail.com>
2024-11-26 00:15:41 +1100

    Added analysis type selection and specific prompts

--- aisetup.py
+++ aisetup.py
@@ -1,10 +1,20 @@
+# aisetup.py
 from openai import OpenAI
 
+# Make client global
+client = None
+
 def authenticate(api_key):
+    """Initialize global OpenAI client"""
     global client
     client = OpenAI(api_key=api_key)
 
 def get_llm_response(prompt):
+    """Get response from OpenAI"""
+    global client
+    if client is None:
+        raise Exception("Please authenticate first")
+    
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}]
